b894fc4455571ae9e792a253351986b0976ddf7d
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-12-06 22:36:40 +0100

    created a random number generator

---
+++ index.js
@@ -0,0 +1,21 @@
+const generatedNumber = Math.floor(Math.random() * 100) + 1;
+
+let attempts = 0;
+
+function checkGuess() {
+  const guessInput = document.getElementById("guess");
+  const message = document.getElementById("message");
+
+  const userGuess = parseInt(guessInput.value);
+  attempts++;
+
+
+  if (userGuess === generatedNumber) {
+    message.textContent = `Congratulations! You guessed the number ${generatedNumber} correctly in ${attempts} attempts.`;
+  } else if (userGuess < generatedNumber) {
+    message.textContent = "Too low! Try again.";
+  } else {
+    message.textContent = "Too high! Try again.";
+  }
+  guessInput.value = "";
+}
