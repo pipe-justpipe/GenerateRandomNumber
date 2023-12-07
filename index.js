6fc5d50d74aedbabf3d7c319ddae3896c5918cd7
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-12-07 15:30:46 +0100

    make me green

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-let attempts = 0;
-
 function confirmGuess(myGuess) {
-    const generatedNumber = Math.floor(Math.random() * 100) + 1;
-    attempts++;
-    //Math.floor() rounds down a decimal number to the nearest integer 
-    //Math.random() generates a random number
-    //Adding 1 ensures that the range start from 1
-        console.log(`Generated number: ${generatedNumber}`);
-    //I used template literals to interpolate string and embed the expression inside the string literals using backticks (``) and dollar sign ($)    
+  const generatedNumber = Math.floor(Math.random() * 100) + 1;
 
-    if (myGuess === generatedNumber) {
-        console.log(`Yay! You guessed the number ${generatedNumber} correctly in ${attempts} attempts.`);
-    } else if (myGuess < generatedNumber) {
-        console.log('Too low! Try again.');
-    } else {
-        console.log('Too high! Try again.');
-    }
+//Math.floor() rounds down a decimal number to the nearest integer
+//Math.random() generates a random number
+//Adding 1 ensures that the range start from 1
+  console.log(`Generated number: ${generatedNumber}`);
+  if (myGuess === generatedNumber) {
+    return `Yay! You guessed the number ${generatedNumber} correctly`;
+  } else if (myGuess < generatedNumber) {
+    return "Your guess is too low! Please try again.";
+  } else {
+    return "Your guess is too high! Please try again.";
+  }
 }
 
-confirmGuess(12);
+const result = confirmGuess(16);
+console.log(result);
+
+//// for green git
