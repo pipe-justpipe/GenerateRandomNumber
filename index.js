446cd5d80123a4bd54205bf3ab122c0540586403
Aremo Pipeloluwa <118048136+pipe-justpipe@users.noreply.github.com>
2023-12-07 07:10:44 +0100

    Update index.js
    I updated script

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 const generatedNumber = Math.floor(Math.random() * 100) + 1;
-//I used Math.floor to round any randomly generated float down to the nearest integer. 
 let attempts = 0;
 
-function checkGuess() {
-  const guessInput = document.getElementById("guess");
-  const message = document.getElementById("message");
+function confirmGuess(myGuess) {
+    attempts++;
 
-  const userGuess = parseInt(guessInput.value);
-  //I used parseInt to convert the user's input to a number because it was recognised as a string
-  attempts++;
-
-
-  if (userGuess === generatedNumber) {
-    message.textContent = `Yay! You guessed the number ${generatedNumber} correctly in ${attempts} attempts.`;
-    //I used template literals for string interpolation with the generated number and the number of attempts
-  } else if (userGuess < generatedNumber) {
-    message.textContent = "Too low! Try again.";
-  } else {
-    message.textContent = "Too high! Try again.";
-  }
-  // I used the .value = "" (empty string) to clear the input
-  guessInput.value = "";
-
+    if (myGuess === generatedNumber) {
+        console.log(`Yay! You guessed the number ${generatedNumber} correctly in ${attempts} attempts.`);
+    } else if (myGuess < generatedNumber) {
+        console.log('Too low! Try again.');
+    } else {
+        console.log('Too high! Try again.');
+    }
 }
+
+confirmGuess(50); 
+
