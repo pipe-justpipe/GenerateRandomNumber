402850c96da84299a08249f10d2dd41df734fafb
pipe-justpipe <aremopipeloluwa@gmail.com>
2023-12-07 09:13:49 +0100

    updated script

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let attempts = 0;
 function confirmGuess(myGuess) {
     const generatedNumber = Math.floor(Math.random() * 100) + 1;
     attempts++;
-//Math.floor() rounds down a decimal number to the nearest integer 
-//Math.random() generates a random number
-//Adding 1 ensures that the range start from 1
-    console.log(`Generated number: ${generatedNumber}`);
+    //Math.floor() rounds down a decimal number to the nearest integer 
+    //Math.random() generates a random number
+    //Adding 1 ensures that the range start from 1
+        console.log(`Generated number: ${generatedNumber}`);
+    //I used template literals to interpolate string and embed the expression inside the string literals using backticks (``) and dollar sign ($)    
 
     if (myGuess === generatedNumber) {
         console.log(`Yay! You guessed the number ${generatedNumber} correctly in ${attempts} attempts.`);
@@ -17,4 +18,4 @@ function confirmGuess(myGuess) {
     }
 }
 
-confirmGuess(50);
+confirmGuess(12);
